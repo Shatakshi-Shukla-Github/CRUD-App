97189a35f1f1fa1277fa3608886aadf26fe5ad3c
Shatakshi Shukla <shatakshishukla49@gmail.com>
2025-09-14 14:29:52 +0530

    Added the UUID package to uniquely identify the id of the comments

--- index.js
+++ index.js
@@ -5,25 +5,25 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-
+const { v4: uuid } = require('uuid');
 const comments = [
     {
-        id: 1,
+        id: uuid(),
         username: 'Todd',
         comment: 'Hey Im Todd, this recipe sucks'
     },
     {
-        id: 2,
+        id: uuid(),
         username: 'Pentol',
         comment: 'I am Cuter than you'
     },
     {
-        id: 3,
+        id: uuid(),
         username: 'Shatovic',
         comment: 'That is so funny'
     },
     {
-        id: 4,
+        id: uuid(),
         username: 'TammyThunderBalls',
         comment: 'My name sounds like a german verse to slay a dragon'
     }
@@ -33,7 +33,7 @@ app.get('/comments/new', (req, res) => {
 })
 app.post('/comments', (req, res) => {
     const { username, comment } = req.body
-    comments.push({ username, comment });
+    comments.push({ username, comment, id: uuid() });
     res.redirect('/comments');
 })
 app.get('/comments', (req, res) => {
@@ -41,7 +41,7 @@ app.get('/comments', (req, res) => {
 })
 app.get('/comments/:id', (req, res) => {
     const { id } = req.params;
-    const comment = comments.find(c => c.id === parseInt(id))
+    const comment = comments.find(c => c.id === id)
     res.render('comments/show', { comment });
 })
 // app.get('/tacos', (req, res) => {
