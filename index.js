90967bfd86c30aaa6c9d44fb701a07dceb104932
Shatakshi Shukla <shatakshishukla49@gmail.com>
2025-09-19 20:44:38 +0530

    Adding the PATCH Request Functionality for modifying the comments

--- index.js
+++ index.js
@@ -44,6 +44,13 @@ app.get('/comments/:id', (req, res) => {
     const comment = comments.find(c => c.id === id)
     res.render('comments/show', { comment });
 })
+app.patch('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    const newCommentText = req.body.comment;
+    const foundComment = comments.find(c => c.id === id);
+    foundComment.comment = newCommentText;
+    res.redirect('/comments');
+})
 // app.get('/tacos', (req, res) => {
 //     res.send("GET /tacos response");
 // })
